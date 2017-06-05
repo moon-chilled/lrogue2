a583d0fcb96a950785bca3a1e9039c88978b876c
Elronnd <elronnd@slashem.me>
2017-06-05 01:56:09 -0700

    Renovate rng

--- src/random.c
+++ src/random.c
@@ -1,79 +1,103 @@
-long rrandom();
+#include <stdint.h>
 
-static long rntb[32] = {
-	         3, 0x9a319039, 0x32d9c024, 0x9b663182, 0x5da1f342, 
-	0xde3b81e0, 0xdf0a6fb5, 0xf103bc02, 0x48f340fb, 0x7449e56b,
-	0xbeb1dbb0, 0xab5c5918, 0x946554fd, 0x8c2e680f, 0xeb3d799f,
-	0xb11ee0b7, 0x2d436b86, 0xda672e2a, 0x1588ca88, 0xe369735d,
-	0x904f35f7, 0xd7158fd6, 0x6fa6f051, 0x616e6b96, 0xac94efdc, 
-	0x36413f93, 0xc622c298, 0xf5a42ab8, 0x8a88d77b, 0xf5ad9d0e,
-	0x8999220b, 0x27fb47b9
+// Define MT19937 constants (32-bit RNG)
+enum {
+	// Assumes W = 32 (omitting this)
+	N = 624,
+	M = 397,
+	R = 31,
+	A = 0x9908B0DF,
+
+	F = 1812433253,
+
+	U = 11,
+	// Assumes D = 0xFFFFFFFF (omitting this)
+
+	S = 7,
+	B = 0x9D2C5680,
+
+	T = 15,
+	C = 0xEFC60000,
+
+	L = 18,
+
+	MASK_LOWER = (1ull << R) - 1,
+	MASK_UPPER = (1ull << R)
 };
 
-static long *fptr = &rntb[4];
-static long *rptr = &rntb[1];
-static long *state = &rntb[1];
-static int rand_type = 3;
-static int rand_deg = 31;
-static int rand_sep = 3;
-static long *end_ptr = &rntb[32];
 
-void srrandom(unsigned long x) {
-	register int i;
+static uint32_t mt[N];
+static uint16_t index;
 
-	state[0] = (long) x;
-	if (rand_type != 0) {
-		for (i = 1; i < rand_deg; i++) {
-			state[i] = 1103515245 * state[i - 1] + 12345;
-		}
-		fptr = &state[rand_sep];
-		rptr = &state[0];
-		for (i = 0; i < 10*rand_deg; i++) {
-			rrandom();
-		}
+// Re-init with a given seed
+void srrandom(const uint32_t seed) {
+	uint32_t i;
+
+	mt[0] = seed;
+
+	for (i = 1; i < N; i++) {
+		mt[i] = (F * (mt[i - 1] ^ (mt[i - 1] >> 30)) + i);
 	}
+
+	index = N;
 }
 
-long rrandom() {
-	long i;
-	
-	if (rand_type == 0) {
-		i = state[0] = (state[0]*1103515245 + 12345) & 0x7fffffff;
-	} else {
-		*fptr += *rptr;
-		i = (*fptr >> 1) & 0x7fffffff;
-		if (++fptr >= end_ptr) {
-			fptr = state;
-			++rptr;
-		} else {
-			if (++rptr >= end_ptr) {
-				rptr = state;
-			}
+static void twist_mt() {
+	uint32_t  i, x, xA;
+
+	for (i = 0; i < N; i++) {
+		x = (mt[i] & MASK_UPPER) + (mt[(i + 1) % N] & MASK_LOWER);
+
+		xA = x >> 1;
+
+		if (x & 0x1) {
+			xA ^= A;
 		}
+
+		mt[i] = mt[(i + M) % N] ^ xA;
 	}
-	return i;
+
+	index = 0;
 }
 
-int get_rand(int x, int y) {
-    int r, t;
-    long lr;
+// Obtain a 32-bit random number
+uint32_t rrandom() {
+	uint32_t y;
+	int i = index;
 
-    if (x > y) {
-        t = y;
-        y = x;
-        x = t;
-    }
-    lr = rrandom();
-    lr &= (long) 0x00007fff;
-    r = (int) lr;
-    r = (r % ((y - x) + 1)) + x;
-    return r;
+	if (index >= N) {
+		twist_mt();
+		i = index;
+	}
+
+	y = mt[i];
+	index = i + 1;
+
+	y ^= (mt[i] >> U);
+	y ^= (y << S) & B;
+	y ^= (y << T) & C;
+	y ^= (y >> L);
+
+	return y;
 }
 
-int rand_percent(int percentage) {
+uint32_t get_rand(uint32_t min, uint32_t max) {
+	uint32_t tmp;
+
+	if (min > max) {
+		tmp = max;
+		max = min;
+		min = tmp;
+	}
+
+	return (rrandom() % ((max - min) + 1)) + min;
+}
+
+char rand_percent(int percentage) {
 	return get_rand(1, 100) <= percentage;
 }
 
 char coin_toss() {
-	return (rrandom() & 1);
+	// >> 8 is to discard the low-order bits.  Just in case.
+	return ((rrandom() >> 8) & 1);
 }
